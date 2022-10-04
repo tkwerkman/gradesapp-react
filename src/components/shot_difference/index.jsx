6bbb24e9f252a71d2b87207f986e5db2deb2823e
tkwerkman <104406689+tkwerkman@users.noreply.github.com>
2022-10-04 14:11:54 -0600

    added arrows to shot display

--- src/components/shot_difference/index.jsx
+++ src/components/shot_difference/index.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
+import { UpArrow, DownArrow } from '../../content/icons/Arrows';
 
 export function Difference({ num1, num2, color }) {
-
-  const diff = (num2 - num1);
-	const rnd = Math.round((diff*100))/100
+  const diff = num2 - num1;
+  const rnd = Math.round(diff * 100) / 100;
   let str = '' + Math.abs(rnd);
+  let Ret;
 
   if (diff < 0) {
-    str += 'm +';
+    str += 'm';
+    Ret = (
+      <div className='flex' style={{ color: color }}>
+        <UpArrow />
+        {str}
+      </div>
+    );
   } else if (diff > 0) {
-    str += 'm -';
+    str += 'm';
+    Ret = (
+      <div className='flex' style={{ color: color }}>
+        <DownArrow />
+        {str}
+      </div>
+    );
   } else if (diff === 0) {
-    str = '00';
+    str = Ret = (
+      <div
+        style={{
+          textDecoration: str === '00' ? 'line-through' : 'none',
+          color: color,
+        }}
+      >
+        {str}
+      </div>
+    );
   }
 
-  return (
-    <span style={{ textDecoration: str === '00' ? 'line-through' : 'none', color: color }}>
-      {str}
-    </span>
-  );
+  return Ret;
 }
