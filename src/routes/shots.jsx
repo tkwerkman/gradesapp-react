64af8bc98e43d9a9d182d40b93222324dffffe3f
tkwerkman <104406689+tkwerkman@users.noreply.github.com>
2022-10-03 21:53:29 -0600

    rewrite of shot visualizer to make it... better.

--- src/routes/shots.jsx
+++ src/routes/shots.jsx
@@ -48,19 +48,24 @@ export default function Shots() {
 
 			<div className="shot-stick-container">
 				<svg className="absolute top-0 left-0 border-[1px] border-red-500" height="100%" width="100%">
-					<line style={{ stroke: "red", strokeWidth: 2 }} x1="20%" y1={pos(num1)} x2="80%" y2={pos(num2)} />
-					<line style={{ stroke: "green", strokeWidth: 2 }} x1="20%" y1={pos(num1)} x2="80%" y2={pos(num1)} />
+					{/* Level line */} <line style={{ stroke: "red", strokeWidth: 2 }} x1="20%" y1={200 - pos(num1)} x2="80%" y2={200 - pos(num1)} />
+					{/* Ground tracking line */} <line style={{ stroke: "orange", strokeWidth: 2 }} x1="25%" y1={200} x2="80.2%" y2={100 + (pos(num2) - pos(num1) + 98)} /> 
 
-					<line style={{ stroke: "blue", strokeDasharray: "4", strokeWidth: "2"}} x1="80%" y1={pos(num2)} x2="85%" y2={pos(num2)} />
-					<line style={{ stroke: "blue", strokeDasharray: "4", strokeWidth: "2"}} x1="80%" y1={pos(num1)} x2="85%" y2={pos(num1)} />
+					{/* Ground illustration line */}<line style={{ stroke: "green", strokeWidth: 2}} x1="15%" y1={200} x2="25%" y2={200} />
 
-					<line style={{ stroke: "blue", strokeWidth: 2 }} x1="85%" y1={pos(num2)} x2="85%" y2={pos(num1)} />
+					{/* Top dotted line */} <line style={{ stroke: "blue", strokeDasharray: "8", strokeWidth: "2"}} x1="81%" y1={100 + (pos(num2) - pos(num1) + 100)} x2="85%" y2={100 + (pos(num2) - pos(num1) + 100)} />
+					{/* Bottom dotted line */}<line style={{ stroke: "blue", strokeDasharray: "8", strokeWidth: "2"}} x1="25%" y1={200} x2="85%" y2={200} />
+
+					{/* Vertical connector (dotted lines) */}<line style={{ stroke: "blue", strokeWidth: 2 }} x1="85%" y1={100 + (pos(num2) - pos(num1) + 100)} x2="85%" y2={200} />
 				</svg>
 
-				<div id="diffText" className="absolute" style={{top: ((pos(num2) + pos(num1))/2)-10, left: "87%"}}><Difference num1={num1} num2={num2} /></div>
+				<div id="diffText" className="absolute" style={{top: ((100 + (pos(num2) - pos(num1) + 100) + 200)/2)-10, left: "87%"}}><Difference num1={num1} num2={num2} /></div>
 
-				<div id="shot1" className="shot-stick" style={{ left: "20%", top: `${pos(num1)}px` }}></div>
-				<div id="shot2" className="shot-stick" style={{ left: "80%", top: `${pos(num2)}px` }}></div>
+
+
+				<div id="shot1" className="shot-stick" style={{ left: "20%", top: `100px` }} />
+				<div id="shot2" className="shot-stick" style={{ left: "80%", top: `${pos(num2) - pos(num1) + 100}px` }} />
+
 			</div>
 		</div>
 
