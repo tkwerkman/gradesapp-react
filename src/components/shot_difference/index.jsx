7c0e947ad74873e11fb83deb12355ce122c8edff
tkwerkman <104406689+tkwerkman@users.noreply.github.com>
2022-10-04 13:15:29 -0600

    fixed rounding bug

--- src/components/shot_difference/index.jsx
+++ src/components/shot_difference/index.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
-export function Difference({ num1, num2 }) {
-  const diff = Math.floor((num2 - num1) * 100) / 100;
-  let str = '' + Math.abs(diff);
+export function Difference({ num1, num2, color }) {
+
+  const diff = (num2 - num1);
+	const rnd = Math.round((diff*100))/100
+  let str = '' + Math.abs(rnd);
 
   if (diff < 0) {
-    str += 'm higher';
+    str += 'm +';
   } else if (diff > 0) {
-    str += 'm lower';
+    str += 'm -';
   } else if (diff === 0) {
     str = '00';
   }
 
   return (
-    <span style={{ textDecoration: str === '00' ? 'line-through' : 'none' }}>
+    <span style={{ textDecoration: str === '00' ? 'line-through' : 'none', color: color }}>
       {str}
     </span>
   );
