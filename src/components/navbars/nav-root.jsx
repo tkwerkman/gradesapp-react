caa8c149bd01ecd94ca3780f5dd77f332c0c971d
tkwerkman <104406689+tkwerkman@users.noreply.github.com>
2023-08-18 11:19:24 -0600

    removed info section for testing

--- src/components/navbars/nav-root.jsx
+++ src/components/navbars/nav-root.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import PillBar, {
-  ExpandablePill,
-  ExpandablePillChild,
-  Pill,
-} from '../../components/pillbar';
+import PillBar, { ExpandablePill, ExpandablePillChild, Pill } from '../../components/pillbar';
 
 export function NavBarRoot() {
   const navigate = useNavigate();
@@ -15,7 +11,7 @@ export function NavBarRoot() {
     Offsets: '/slopes/offsets',
     Grades: '/slopes/grades',
     Stations: '/info/stations',
-		Equipment: '/info/equipment',
+    Equipment: '/info/equipment',
     FAQ: '/info/faq',
   };
 
@@ -24,24 +20,47 @@ export function NavBarRoot() {
     navigate(route);
   };
 
-  return (
-    <PillBar>
-			<ExpandablePill text='Main'>
-				<ExpandablePillChild text="Home" func={handleDisplay} />
-			</ExpandablePill>
+  const Main = () => {
+    return (
+      <ExpandablePill text='Main'>
+        <ExpandablePillChild text='Home' func={handleDisplay} />
+      </ExpandablePill>
+    );
+  };
+
+  const Shots = () => {
+    return (
       <ExpandablePill text='Shots'>
         <ExpandablePillChild text='Visualizer' func={handleDisplay} />
         <ExpandablePillChild text='Calculator' func={handleDisplay} />
       </ExpandablePill>
+    );
+  };
+
+  const Slopes = () => {
+    return (
       <ExpandablePill text='Slopes'>
         <ExpandablePillChild text='Offsets' func={handleDisplay} />
         <ExpandablePillChild text='Grades' func={handleDisplay} />
       </ExpandablePill>
+    );
+  };
+  const Info = () => {
+    return (
       <ExpandablePill text='Info'>
         <ExpandablePillChild text='Stations' func={handleDisplay} />
-				<ExpandablePillChild text='Equipment' func={handleDisplay} />
+        <ExpandablePillChild text='Equipment' func={handleDisplay} />
         <ExpandablePillChild text='FAQ' func={handleDisplay} />
       </ExpandablePill>
+    );
+  };
+
+  return (
+    <PillBar>
+			<Main />
+      <Shots />
+			<Slopes />
+			{/* <Info /> */}
     </PillBar>
   );
 }
